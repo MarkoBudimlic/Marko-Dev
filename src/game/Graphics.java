a193b952b7b91bd43741649c26b61d92222c74a1
Marko Antonio Budimlic <marko.a.budimlic@gmail.com>
2018-12-04 10:36:16 +0100

    Clean Space Pirates

--- src/game/Graphics.java
+++ src/game/Graphics.java
@@ -27,15 +27,15 @@ public class Graphics {
     }
 
 
-
+    /**DRAWS THE WORLD ON THE SCENE*/
     public void draw(){
-        /**DRAWS THE MODEL ON THE SCENE*/
+
         gc.setFill(Color.BLACK);
         gc.fillRect(0,0,Main.screenX,Main.screenY);
 
         gc.setFill(Color.WHITE);
         for(Star star : world.stars){
-            gc.fillOval(star.getPositionX(),star.getPositionY(),Star.width,Star.height);
+            gc.fillOval(star.getPositionX(),star.getPositionY(),Star.getWidth(),Star.getHeight());
         }
         gc.setFill(Color.YELLOW);
         gc.setFont(Font.font(80));
@@ -43,21 +43,18 @@ public class Graphics {
 
 
 
-        /**draws player object*/
+
         if(world.isStatus() == false){
-            /**YAO MING*/
-            gc.drawImage(explosionModel, world.player.getPositionX()-100, world.player.getPositionY()-100, world.player.getWidth()+200,world.player.getHeight()+200);
-            gc.setFill(Color.YELLOW);
-            gc.drawImage(new Image("wtf.jpg"),350, 300);
-            gc.setFont(Font.font(160));
-            gc.fillText("GAME OVER", 350,200);
+            //GAME OVER SCREEN
+            drawGameOver();
         }
         else if(world.isStatus() == true) {
-
+                //DRAWS PLAYER OBJECT
                 gc.drawImage(playerModel, world.player.getPositionX(), world.player.getPositionY());
 
 
             for (int i = 0; i < 9; i++) {
+                //DRAWS ENEMY OBJECTS
                 if(i < 3){
                     gc.drawImage(enemyModel1,world.enemies.get(i).getPositionX(), world.enemies.get(i).getPositionY(), world.enemies.get(i).getWidth(), world.enemies.get(i).getHeight());
                 }else if(i == 3){
@@ -67,6 +64,7 @@ public class Graphics {
                 }
 
             }
+            //DRAWS MAELSTROM
             gc.setFill(new RadialGradient(0, 0.1, 0.55, 0.45, 0.06, true,
                     CycleMethod.REFLECT,
                     new Stop(0.8, Color.BLACK),
@@ -77,5 +75,13 @@ public class Graphics {
 
 
 
+    }
+
+    private void drawGameOver() {
+        gc.drawImage(explosionModel, world.player.getPositionX()-100, world.player.getPositionY()-100, world.player.getWidth()+200,world.player.getHeight()+200);
+        gc.setFill(Color.YELLOW);
+        gc.drawImage(new Image("wtf.jpg"),350, 300);
+        gc.setFont(Font.font(160));
+        gc.fillText("GAME OVER", 350,200);
     }
 }
